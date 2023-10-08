74ad76283824319774fb4d7a64257944248f9354
mzeenkala23 <tachyonhd@gmail.com>
2023-10-08 19:32:20 +0100

    chore: update types for button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,11 +1,16 @@
-export const Button = ({ children, onClick, ...props }) => {
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export const Button = ({ children, onClick, ...props }: ButtonProps) => {
   return (
     <button
       className="absolute left-2/4 -bottom-1 -translate-x-1/2 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
       onClick={onClick}
       {...props}
     >
-      Translate
+      {children}
     </button>
   );
 };
